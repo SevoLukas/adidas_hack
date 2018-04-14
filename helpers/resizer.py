7e85d7950a43d05f19871bd68bf06178ac7a0ff0
dim1tri <dimitri.herrero@kiwi.com>
2018-04-14 23:21:57 +0200

    edit resizer

--- helpers/resizer.py
+++ helpers/resizer.py
@@ -6,9 +6,9 @@ from PIL import Image
 from io import BytesIO, StringIO
 
 
-s3 = boto3.client('s3', aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
-                        aws_secret_access_key=os.getenv('AWS_SECRET'),
-                        region_name='eu-west-1')
+s3 = boto3.resource('s3', aws_access_key_id=os.getenv('AWS_ACCESS_KEY', ),
+                          aws_secret_access_key=os.getenv('AWS_SECRET'),
+                          region_name='eu-west-1')
 BUCKET = 'rekognition-adihack'
 
 
