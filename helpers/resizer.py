d181af6e82a7edc9a965b59a06e1273a3b7d99c6
dim1tri <dimitri.herrero@kiwi.com>
2018-04-14 23:04:01 +0200

    edit resizer

--- helpers/resizer.py
+++ helpers/resizer.py
@@ -1,18 +1,33 @@
 import requests
+import boto3
+import os
+import logging
 from PIL import Image
+from io import BytesIO, StringIO
+
+
+s3 = boto3.resource('s3')
+BUCKET = 'rekognition-adihack'
 
 
 def resize_and_upload(url, face_id, height, width, top, left):
-
-    image = requests.get(url)
-    unprocessed_image = Image.open(image)
-    image_width, image_height = unprocessed_image.size
-
-    width_px = image_width * width
-    height_px = image_height * height
-    left_px = image_width * left
-    top_px = image_height * top
-
-    crop = unprocessed_image.crop(left_px, top_px, width_px + left_px, top_px + height_px)
-
-    # TODO: send crop file to S3, along with face_id
+    try:
+        response = requests.get(url)
+        unprocessed_image = Image.open(BytesIO(response.content))
+        image_width, image_height = unprocessed_image.size
+        width = round(image_width * float(width))
+        height = round(image_height * float(height))
+        left = round(image_width * float(left))
+        top = round(image_height * float(top))
+        crop = unprocessed_image.crop((left,
+                                       top,
+                                       width + left,
+                                       top + height))
+        file_name = '{}.jpg'.format(face_id)
+        crop.save(file_name, 'JPEG')
+        with open(file_name, 'rb') as f:
+            s3.Bucket(BUCKET).put_object(Body=f, Key='faces/{}.jpg'.format(face_id))
+        os.remove(file_name)
+    except Exception as e:
+        logging.error(e)
+    return 'ok'
