f63548b065987a4074c9b98d6a6292c290ce9e9b
dim1tri <dimitri.herrero@kiwi.com>
2018-04-15 01:45:00 +0200

    add endpoint for heroku

--- api/run.py
+++ api/run.py
@@ -2,8 +2,12 @@ from flask import Flask, request
 from flask import jsonify
 from api.settings import HOST, PORT, DEBUG
 from helpers.resizer import resize_and_upload
+import psycopg2
+import logging
+
 
 app = Flask(__name__)
+conn = psycopg2.connect("dbname='dd5fd1bu74cdkb' user='vonhwrarqlubaj' host='ec2-54-247-81-88.eu-west-1.compute.amazonaws.com' password='cc3766fdc1656b071806c4209eea4273ce16cdf7e5e8050d5fe30a5fbe5e0f7a'")
 
 
 @app.route("/")
@@ -21,6 +25,70 @@ def resize_api():
     left = request.args.get('left')
 
     resize_and_upload(url, face_id, height, width, top, left)
+    return 'ok'
+
+
+@app.route("/get-record", methods=['GET', 'POST'])
+def get_record():
+    cur = conn.cursor()
+    adi_client = request.get_json(silent=True)
+    if adi_client['is_new_user']:
+        query = """
+        INSERT INTO adi_client (gender) VALUES(%s)
+        RETURNING id
+        """
+        cur.execute(query, ('Female',))
+        adi_client_id = cur.fetchone()[0]
+        query = """
+        INSERT INTO adi_face (user_id,
+                              camera_id,
+                              min_age,
+                              max_age,
+                              happy,
+                              sad,
+                              angry,
+                              confused,
+                              disgusted,
+                              surprised,
+                              smile,
+                              calm,
+                              image_url)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """
+        cur.execute(query, (adi_client_id,
+                            adi_client['camera_id'],
+                            adi_client['age']['min'],
+                            adi_client['age']['max'],
+                            adi_client['emotions']['happy'],
+                            adi_client['emotions']['sad'],
+                            adi_client['emotions']['angry'],
+                            adi_client['emotions']['confused'],
+                            adi_client['emotions']['disgusted'],
+                            adi_client['emotions']['surprised'],
+                            adi_client['emotions']['smile'],
+                            adi_client['emotions']['calm'],
+                            adi_client['image_url']))
+        conn.commit()
+        # cur.execute("INSERT INTO product(store_id, url, price, charecteristics, color, dimensions) VALUES (%s, %s, %s, %s, %s, %s)", (1,  'http://www.google.com', '$20', json.dumps(thedictionary), 'red', '8.5x11'))
+
+
+        # """
+
+    # {
+    #     'is_new_user': len(matches) == 0,
+    #     'matches': matches,
+    #     'face_id': face_id,
+    #     'camera_id': get_camera_id(key),
+    #     'age': get_age_info(face_detail),
+    #     'gender': get_gender(face_detail),
+    #     'emotions': get_emotions(face_detail),
+    #     'image_url': FACES_PREFIX + face_id + '.jpg',
+    # }
+
+    # cur.execute("""SELECT * from adi_client""")
+    # rows = cur.fetchall()
+    # for row in rows:
+    #     logging.warning(row)
     return 'ok'
 
 
