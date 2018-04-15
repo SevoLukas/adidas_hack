a1ea9cbbb1fe70c6693c48ffe15f323859f9c0ec
dim1tri <dimitri.herrero@kiwi.com>
2018-04-15 05:04:20 +0200

    fix bugs

--- api/run.py
+++ api/run.py
@@ -1,12 +1,26 @@
-from flask import Flask, request, json
+from contextlib import contextmanager
+
+from flask import Flask, request, json, jsonify
+from psycopg2 import pool
+
 from api.settings import HOST, PORT, DEBUG
 from helpers.resizer import resize_and_upload
-import psycopg2
 import logging
 
-
 app = Flask(__name__)
-conn = psycopg2.connect("dbname='dd5fd1bu74cdkb' user='vonhwrarqlubaj' host='ec2-54-247-81-88.eu-west-1.compute.amazonaws.com' password='cc3766fdc1656b071806c4209eea4273ce16cdf7e5e8050d5fe30a5fbe5e0f7a'")
+db = pool.SimpleConnectionPool(
+    1, 10, host='ec2-54-247-81-88.eu-west-1.compute.amazonaws.com', database='dd5fd1bu74cdkb', user='vonhwrarqlubaj',
+    password='cc3766fdc1656b071806c4209eea4273ce16cdf7e5e8050d5fe30a5fbe5e0f7a', port=5432)
+
+
+@contextmanager
+def get_cursor():
+    con = db.getconn()
+    try:
+        yield con
+    finally:
+        con.commit()
+        db.putconn(con)
 
 
 @app.route("/")
@@ -16,16 +30,16 @@ def hello():
 
 @app.route('/latest-records', methods=['GET'])
 def get_latest_records():
-    cur = conn.cursor()
     query = """
-            SELECT adi_face.*, adi_client.gender FROM adi_face
-            JOIN adi_client
-              ON adi_face.user_id=adi_client.id
-            ORDER BY adi_face.timestamp DESC
-            LIMIT 10
-            """
-    cur.execute(query)
-    results = cur.fetchall()
+    SELECT adi_face.*, adi_client.gender FROM adi_face
+    JOIN adi_client
+      ON adi_face.user_id=adi_client.id
+    ORDER BY adi_face.timestamp DESC
+    LIMIT 10
+    """
+    with get_cursor() as cursor:
+        cursor.execute(query)
+        results = cursor.fetchall()
     data = [{
         'face_id': result[0],
         'user_id': result[1],
@@ -49,6 +63,7 @@ def get_latest_records():
         'timestamp': result[14],
 
     } for result in results]
+    logging.error(data)
     response = app.response_class(
         response=json.dumps(data),
         status=200,
@@ -68,95 +83,114 @@ def resize_api():
     width = request.args.get('width')
     top = request.args.get('top')
     left = request.args.get('left')
+
     resize_and_upload(url, face_id, height, width, top, left)
     return 'ok'
 
 
 @app.route("/get-record", methods=['POST'])
 def get_record():
-    cur = conn.cursor()
     adi_client = request.get_json(silent=True)
     if adi_client['is_new_user']:
         query = """
         INSERT INTO adi_client (gender) VALUES(%s)
         RETURNING id
         """
-        cur.execute(query, (adi_client.get('gender', ''),))
-        adi_client_id = cur.fetchone()[0]
-        query = """
-        INSERT INTO adi_face (id,
-                              user_id,
-                              camera_id,
-                              min_age,
-                              max_age,
-                              happy,
-                              sad,
-                              angry,
-                              confused,
-                              disgusted,
-                              surprised,
-                              smile,
-                              calm,
-                              image_url)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-        """
-        cur.execute(query, (adi_client['face_id'],
-                            adi_client_id,
-                            adi_client['camera_id'],
-                            adi_client['age']['low'],
-                            adi_client['age']['high'],
-                            adi_client['emotions']['happy'],
-                            adi_client['emotions']['sad'],
-                            adi_client['emotions']['angry'],
-                            adi_client['emotions']['confused'],
-                            adi_client['emotions']['disgusted'],
-                            adi_client['emotions']['surprised'],
-                            adi_client['emotions']['smile'],
-                            adi_client['emotions']['calm'],
-                            adi_client['image_url']))
-        conn.commit()
+        with get_cursor() as cursor:
+
+            cursor.execute(query, (adi_client.get('gender', ''),))
+            adi_client_id = cursor.fetchone()[0]
+            query = """
+            INSERT INTO adi_face (id,
+                                  user_id,
+                                  camera_id,
+                                  min_age,
+                                  max_age,
+                                  happy,
+                                  sad,
+                                  angry,
+                                  confused,
+                                  disgusted,
+                                  surprised,
+                                  smile,
+                                  calm,
+                                  image_url)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            """
+            cursor.execute(query, (adi_client['face_id'],
+                                   adi_client_id,
+                                   adi_client['camera_id'],
+                                   adi_client['age']['low'],
+                                   adi_client['age']['high'],
+                                   adi_client['emotions']['happy'],
+                                   adi_client['emotions']['sad'],
+                                   adi_client['emotions']['angry'],
+                                   adi_client['emotions']['confused'],
+                                   adi_client['emotions']['disgusted'],
+                                   adi_client['emotions']['surprised'],
+                                   adi_client['emotions']['smile'],
+                                   adi_client['emotions']['calm'],
+                                   adi_client['image_url']))
     else:
         for match in adi_client['matches']:
             query = "SELECT user_id FROM adi_face WHERE id='{}'".format(match)
-            cur.execute(query)
-            matched_ids = cur.fetchone()
-            if matched_ids is not None:
-                user_id = matched_ids[0]
-                query = """
-                        INSERT INTO adi_face (id,
-                                              user_id,
-                                              camera_id,
-                                              min_age,
-                                              max_age,
-                                              happy,
-                                              sad,
-                                              angry,
-                                              confused,
-                                              disgusted,
-                                              surprised,
-                                              smile,
-                                              calm,
-                                              image_url)
-                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-                        """
-                cur.execute(query, (adi_client['face_id'],
-                                    user_id,
-                                    adi_client['camera_id'],
-                                    adi_client['age']['low'],
-                                    adi_client['age']['high'],
-                                    adi_client['emotions']['happy'],
-                                    adi_client['emotions']['sad'],
-                                    adi_client['emotions']['angry'],
-                                    adi_client['emotions']['confused'],
-                                    adi_client['emotions']['disgusted'],
-                                    adi_client['emotions']['surprised'],
-                                    adi_client['emotions']['smile'],
-                                    adi_client['emotions']['calm'],
-                                    adi_client['image_url']))
-                conn.commit()
-            else:
-                continue
+            with get_cursor() as cursor:
+
+                cursor.execute(query)
+                matched_ids = cursor.fetchone()
+                if matched_ids is not None:
+                    user_id = matched_ids[0]
+                    query = """
+                            INSERT INTO adi_face (id,
+                                                  user_id,
+                                                  camera_id,
+                                                  min_age,
+                                                  max_age,
+                                                  happy,
+                                                  sad,
+                                                  angry,
+                                                  confused,
+                                                  disgusted,
+                                                  surprised,
+                                                  smile,
+                                                  calm,
+                                                  image_url)
+                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                            """
+                    cursor.execute(query, (adi_client['face_id'],
+                                           user_id,
+                                           adi_client['camera_id'],
+                                           adi_client['age']['low'],
+                                           adi_client['age']['high'],
+                                           adi_client['emotions']['happy'],
+                                           adi_client['emotions']['sad'],
+                                           adi_client['emotions']['angry'],
+                                           adi_client['emotions']['confused'],
+                                           adi_client['emotions']['disgusted'],
+                                           adi_client['emotions']['surprised'],
+                                           adi_client['emotions']['smile'],
+                                           adi_client['emotions']['calm'],
+                                           adi_client['image_url']))
+                else:
+                    continue
     return 'ok'
+
+
+@app.route("/recommend", methods=["POST"])
+def recommend_product():
+    age = request.args.get('age')
+    gender = request.args.get('gender')
+    gender_code = 1 if gender == 'male' else 2
+    accuracy = 100
+    if gender_code == 1:
+        product = 'BR6930'
+    else:
+        if age < 40:
+            product = 'BY8745'
+        else:
+            product = 'CV9889'
+
+    return jsonify({'product': product, 'accuracy': accuracy})
 
 
 if __name__ == '__main__':
