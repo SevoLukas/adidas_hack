f47b60c2f8e7a11229847de35eb102bb88129bfb
dim1tri <dimitri.herrero@kiwi.com>
2018-04-15 03:37:14 +0200

    fix bug

--- api/run.py
+++ api/run.py
@@ -1,8 +1,4 @@
 from flask import Flask, request, json, jsonify
-from flask_cors import CORS, cross_origin
-from flask import make_response, request, current_app
-from functools import update_wrapper
-from datetime import timedelta
 from api.settings import HOST, PORT, DEBUG
 from helpers.resizer import resize_and_upload
 import psycopg2
@@ -10,50 +6,7 @@ import logging
 
 
 app = Flask(__name__)
-
 conn = psycopg2.connect("dbname='dd5fd1bu74cdkb' user='vonhwrarqlubaj' host='ec2-54-247-81-88.eu-west-1.compute.amazonaws.com' password='cc3766fdc1656b071806c4209eea4273ce16cdf7e5e8050d5fe30a5fbe5e0f7a'")
-
-
-def crossdomain(origin=None, methods=None, headers=None,
-                max_age=21600, attach_to_all=True,
-                automatic_options=True):
-    if methods is not None:
-        methods = ', '.join(sorted(x.upper() for x in methods))
-    if headers is not None and not isinstance(headers, str):
-        headers = ', '.join(x.upper() for x in headers)
-    if not isinstance(origin, str):
-        origin = ', '.join(origin)
-    if isinstance(max_age, timedelta):
-        max_age = max_age.total_seconds()
-
-    def get_methods():
-        if methods is not None:
-            return methods
-
-        options_resp = current_app.make_default_options_response()
-        return options_resp.headers['allow']
-
-    def decorator(f):
-        def wrapped_function(*args, **kwargs):
-            if automatic_options and request.method == 'OPTIONS':
-                resp = current_app.make_default_options_response()
-            else:
-                resp = make_response(f(*args, **kwargs))
-            if not attach_to_all and request.method != 'OPTIONS':
-                return resp
-
-            h = resp.headers
-
-            h['Access-Control-Allow-Origin'] = origin
-            h['Access-Control-Allow-Methods'] = get_methods()
-            h['Access-Control-Max-Age'] = str(max_age)
-            if headers is not None:
-                h['Access-Control-Allow-Headers'] = headers
-            return resp
-
-        f.provide_automatic_options = False
-        return update_wrapper(wrapped_function, f)
-    return decorator
 
 
 @app.route("/")
@@ -61,8 +14,7 @@ def hello():
     return "Hello World!"
 
 
-@app.route('/api/latest-records', methods=['GET'])
-@crossdomain(origin='*')
+@app.route('/latest-records', methods=['GET'])
 def get_latest_records():
     cur = conn.cursor()
     query = """
@@ -104,6 +56,8 @@ def get_latest_records():
         mimetype='application/json'
     )
     response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET,PUT,POST,DELETE,OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization, Content-Length, X-Requested-With'
     return response
 
 
